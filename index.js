eb934b2fdcfb4e67428609b760521e0b53d775ea
Muammar Zaidan Ariq <zaidanariq503@gmail.com>
2024-01-13 08:07:17 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer();
 
 var app = express();
 
@@ -8,13 +10,21 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+	res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+	const file = req.file;
+	const fileName = req.body.upfile;
+	const dataFile = {
+		name: file.originalname,
+		type: file.mimetype,
+		size: file.size,
+	};
+	res.json(dataFile);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+	console.log('Your app is listening on port ' + port);
 });
